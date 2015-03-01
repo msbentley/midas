msbentley/midas

commit e987cde0ac6d31afe040e8a633f6179b2f39f555

In plot_fscans() only try to plot thresholds if those columns exist

midas/ros_tm.py
@@ -191,11 +191,12 @@ def plot_fscan(fscans, showfit=False, legend=True, cantilever=None, xmin=False, 
             (scan.excitation, scan.gain, scan.freq_start, scan.freq_step, scan.max_amp, scan.max_freq),
             fontsize=12 )
 
-        # Also drawn lines showing the working point and set point
-        ax.axhline(scan.res_amp,color='b')
-        ax.axhline(scan.work_pt,color='r')
-        ax.axhline(scan.set_pt,color='g')
-        ax.axhline(scan.fadj,color='g', ls='--')
+        if set(['res_amp','work_pt', 'set_pt', 'fadj']).issubset(set(scan.keys())):
+            # Also drawn lines showing the working point and set point
+            ax.axhline(scan.res_amp,color='b')
+            ax.axhline(scan.work_pt,color='r')
+            ax.axhline(scan.set_pt,color='g')
+            ax.axhline(scan.fadj,color='g', ls='--')
 
     # ax.set_xlim()
     ax.set_ylim(0)

commit 442153d5c8268edae3c0b5911883b557bd163961

Fixing frequency scan threshold extraction (broke in e987cde0)

midas/ros_tm.py
@@ -2750,11 +2750,12 @@ class tm:
             # take the scan info from the first packet where generic to entire scan
             scan = {}
             scan['info'] = {}
+            start_time = obt_epoch + timedelta(seconds=first_pkt.start_time)
 
             # Look at HK2 data to retrieve the resonance amplitude, working point, frequency
             # adjust point and set-point derived from this scan...
             if get_thresh:
-                frame = hk2[hk2.obt>start].index
+                frame = hk2[hk2.obt>start_time].index
                 if len(frame)==0:
                     print('WARNING: no HK2 frame found after frequency scan at %s' % start)
                     continue
@@ -2763,7 +2764,7 @@ class tm:
 
             scan['info'] = {
                 'sw_ver': '%i.%i.%i' % (first_pkt.sw_major & 0x0F, first_pkt.sw_major >> 4, first_pkt.sw_minor),
-                'start_time': obt_epoch + timedelta(seconds=first_pkt.start_time),
+                'start_time': start_time,
                 'freq_start': first_pkt.freq_start * 0.0006984, # 00021065027,
                 'freq_step': first_pkt.freq_step * 0.0006984, # 00021065027,
                 'max_amp': single_scan.max_amp.max()*(20./65535.),
3af9a4047eba4ed38a28290f2515836b4b2f1048
Mark S. Bentley <mark@lunartech.org>
2015-07-10 21:22:12 +0200

    on-disk() option added to read data per packet instead of reading TLM file into memory

--- midas/archive.py
+++ midas/archive.py
@@ -108,7 +108,7 @@ def search_params(search=''):
 
 
 
-def read_data(files, apid, sid, calibrate=False, tsync=True, use_index=False):
+def read_data(files, apid, sid, calibrate=False, tsync=True, use_index=False, on_disk=False):
     """Read in data for a given APID and SID and return a dataframe of calibrated
     data for all matching frames. This can then be written to an archive.
 
@@ -148,8 +148,11 @@ def read_data(files, apid, sid, calibrate=False, tsync=True, use_index=False):
         if debug: print('DEBUG: processing file %s' % f)
 
         # Open each file as a bitstream and filter the packet list to frames from this packet
-        # s = ConstBitStream(filename=f)
-        s = ConstBitStream(bytes=open(f, 'rb').read(), length=os.path.getsize(f)*8)
+        if on_disk:
+            s = ConstBitStream(filename=f)
+        else:
+            s = ConstBitStream(bytes=open(f, 'rb').read(), length=os.path.getsize(f)*8)
+
         frames = pkts[pkts.filename==f]
         frames.reset_index(inplace=True)
 
@@ -193,7 +196,7 @@ def read_data(files, apid, sid, calibrate=False, tsync=True, use_index=False):
     return hk_data
 
 
-def create(files='TLM__MD_M*.DAT', tlm_path=common.tlm_path, archive_path=common.tlm_path, archfile='tm_archive_raw.h5', calibrate=False, use_index=False):
+def create(files='TLM__MD_M*.DAT', tlm_path=common.tlm_path, archive_path=common.tlm_path, archfile='tm_archive_raw.h5', calibrate=False, use_index=False, on_disk=False):
     """Writes a DataFrame of (optionally) calibrated TM data to an hdf5 archive"""
 
     # data.to_hdf(hdf5file, pkt_name, mode='w', format='table', data_columns=True)
@@ -210,18 +213,18 @@ def create(files='TLM__MD_M*.DAT', tlm_path=common.tlm_path, archive_path=common
 
         for f in files:
 
-            hk = read_data(files=f, apid=apid, sid=1, calibrate=calibrate)
+            hk = read_data(files=f, apid=apid, sid=1, calibrate=calibrate, on_disk=on_disk)
             store.append('HK1', hk, format='table', data_columns=True, min_itemsize=hk._metadata[2], index=False)
 
-            hk = read_data(files=f, apid=apid, sid=2, calibrate=calibrate)
+            hk = read_data(files=f, apid=apid, sid=2, calibrate=calibrate, on_disk=on_disk)
             store.append('HK2', hk, format='table', data_columns=True, min_itemsize=hk._metadata[2], index=False)
 
     else:
 
-        hk = read_data(files=None, apid=apid, sid=1, calibrate=calibrate, use_index=True)
+        hk = read_data(files=None, apid=apid, sid=1, calibrate=calibrate, use_index=True, on_disk=on_disk)
         store.append('HK1', hk, format='table', data_columns=True, min_itemsize=hk._metadata[2], index=False)
 
-        hk = read_data(files=None, apid=apid, sid=2, calibrate=calibrate, use_index=True)
+        hk = read_data(files=None, apid=apid, sid=2, calibrate=calibrate, use_index=True, on_disk=on_disk)
         store.append('HK2', hk, format='table', data_columns=True, min_itemsize=hk._metadata[2], index=False)
 
     store.root._v_attrs.calibrated = calibrate
@@ -238,7 +241,7 @@ def create(files='TLM__MD_M*.DAT', tlm_path=common.tlm_path, archive_path=common
     return
 
 
-def append(tlm_files='TLM__MD_M*.DAT', tlm_path=common.tlm_path, archive_path=common.tlm_path, archfile='tm_archive.h5', repack=True):
+def append(tlm_files='TLM__MD_M*.DAT', tlm_path=common.tlm_path, archive_path=common.tlm_path, archfile='tm_archive.h5', repack=True, on_disk=False):
     """Appends data in HK packets contained in tlm_path/tlm_files to archive_path/archfile. Only packets with
     OBTs after the final entry in the archive file will be added! Calibration status is maintained."""
 
@@ -249,16 +252,18 @@ def append(tlm_files='TLM__MD_M*.DAT', tlm_path=common.tlm_path, archive_path=co
     store = HDFStore(os.path.join(archive_path,archfile), 'a')
     calibrated = True if store.root._v_attrs.calibrated else False
 
-    hk = read_data(files=os.path.join(tlm_path,tlm_files), apid=apid, sid=1, calibrate=calibrated)
+    hk = read_data(files=os.path.join(tlm_path,tlm_files), apid=apid, sid=1, calibrate=calibrated, on_disk=on_disk)
     metadata = hk._metadata
     obt_max = store.select_column('HK1','index').max()
     hk = hk[hk.index>obt_max]
-    store.append('HK1', hk, format='table', data_columns=True, min_itemsize=metadata[2], index=False)
+    print('INFO: adding %i new HK1 frames to the archive' % len(hk))
+    store.append('HK1', hk, format='table', data_columns=True, min_itemsize=metadata[2], index=False, on_disk=on_disk)
 
     hk = read_data(files=os.path.join(tlm_path,tlm_files), apid=apid, sid=2, calibrate=calibrated)
     metadata = hk._metadata
     obt_max = store.select_column('HK2','index').max()
     hk = hk[hk.index>obt_max]
+    print('INFO: adding %i new HK2 frames to the archive' % len(hk))
     store.append('HK2', hk, format='table', data_columns=True, min_itemsize=metadata[2], index=False)
 
     store.create_table_index('HK1',columns=['index'],optlevel=9,kind='full')

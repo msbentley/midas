c8d29307202fa54474598055b8e411c11f6d052a
Mark S. Bentley <mark@lunartech.org>
2015-11-04 12:39:26 +0100

    First commit of some MPL helper routines

---
+++ midas/plotutils.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+"""plotutils.py
+
+A few routines to help MPL plotting
+
+Mark S. Bentley (mark@lunartech.org), 2015"""
+
+# Some functions to try and fix the broken MPL 3D capability
+
+from mpl_toolkits.mplot3d import proj3d
+
+def orthogonal_proj(zfront, zback):
+    """See https://stackoverflow.com/questions/23840756/how-to-disable-perspective-in-mplot3d"""
+
+    a = (zfront+zback)/(zfront-zback)
+    b = -2*(zfront*zback)/(zfront-zback)
+    # -0.0001 added for numerical stability as suggested in:
+    # http://stackoverflow.com/questions/23840756
+    return numpy.array([[1,0,0,0],
+                        [0,1,0,0],
+                        [0,0,a,b],
+                        [0,0,-0.0001,zback]])
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+
+    See: https://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = x_limits[1] - x_limits[0]; x_mean = np.mean(x_limits)
+    y_range = y_limits[1] - y_limits[0]; y_mean = np.mean(y_limits)
+    z_range = z_limits[1] - z_limits[0]; z_mean = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.3*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_mean - plot_radius, x_mean + plot_radius])
+    ax.set_ylim3d([y_mean - plot_radius, y_mean + plot_radius])
+    ax.set_zlim3d([z_mean - plot_radius, z_mean + plot_radius])

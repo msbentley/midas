b68a67c474a587604ec72991e943e73e259e6f24
Mark S. Bentley <mark@lunartech.org>
2016-03-07 21:48:14 +0100

    Check images for None instead of False when no images are returned

--- midas/midas_daily.py
+++ midas/midas_daily.py
@@ -311,7 +311,7 @@ def regenerate(what='all', files='TLM__MD_M*.DAT', from_index=False):
         if from_index:
             images = ros_tm.load_images(data=True)
             # Save BCR and GWY files
-            if type(images)!=bool:
+            if images is not None:
                 ros_tm.save_gwy(images,os.path.join(image_dir, 'gwy/'), save_png=True, pngdir=os.path.join(image_dir, 'png/')) # and Gwyddion files
                 ros_tm.save_bcr(images.quer('channel=="ZS"'),os.path.join(image_dir, 'bcr/'), write_meta=True) # save images as BCRs + meta data
 
@@ -326,7 +326,7 @@ def regenerate(what='all', files='TLM__MD_M*.DAT', from_index=False):
                 images = tm.get_images(info_only=False, expand_params=True)
 
                 # Save BCR and GWY files
-                if type(images)!=bool:
+                if images is not None:
                     ros_tm.save_bcr(images.query('channel=="ZS"'),os.path.join(image_dir, 'bcr/'), write_meta=True) # save images as BCRs + meta data
                     ros_tm.save_gwy(images,os.path.join(image_dir, 'gwy/'), save_png=True, pngdir=os.path.join(image_dir, 'png/')) # and Gwyddion files
 
